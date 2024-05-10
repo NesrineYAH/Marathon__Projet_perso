6e2f2d95f7918ad286407b012a103deed6cb5f70
NesrineYAH <nesrineyahoum@gmail.com>
2024-05-11 00:33:52 +0200

    //

--- starterOnly/contact.js
+++ starterOnly/contact.js
@@ -1,28 +1,6 @@
-// Récupération du formulaire
-const form = document.getElementById("contactForm");
+const formData = document.querySelectorAll(".formData");
+const ContNodelt = document.querySelector(".Nodelt");
 
-// Gestionnaire d'événement pour le formulaire
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // Empêcher l'envoi par défaut du formulaire
-
-  // Validation du formulaire
-  const isFormValid = validate(form);
-
-  if (isFormValid) {
-    alert("Votre message a été envoyé avec succès !");
-    console.log("Nom :", form["firstName"].value);
-    console.log("Prénom :", form["lastName"].value);
-    console.log("E-mail :", form["email"].value);
-    console.log("Message :", form["message"].value);
-
-    // Réinitialiser le formulaire après l'envoi
-    form.reset();
-  } else {
-    alert(
-      "Certaines informations sont incorrectes. Veuillez les corriger et réessayer."
-    );
-  }
-});
 // Fonction de validation du prénom
 function validateFirst(form) {
   const firstName = form["firstName"].value.trim();
@@ -86,10 +64,10 @@ function validateEmail(form) {
 // Fonction de validation du message
 function validateMessage(form) {
   const message = form["message"].value.trim();
-  //const regexMessage = /^[a-zA-Z\s]+$/;
-  const regexMessage = /^[a-zA-ZÀ-ÿ]{2,35}([-' ,][a-zA-ZÀ-ÿ]+)*$/i;
+  const regexMessage = /^[a-zA-Z\s]+$/;
+  // const regexMessage = /^[a-zA-ZÀ-ÿ]{2,35}([-' ,][a-zA-ZÀ-ÿ]+)*$/i;
 
-  if (message === "" || !regexMessage.test(message)) {
+  if (message === "" || regexMessage.test(message)) {
     const errorField = form.querySelector(".message");
     errorField.setAttribute("data-error-visible", "true");
     errorField.setAttribute("data-error", "Texte du message invalide.");
@@ -102,8 +80,34 @@ function validateMessage(form) {
   }
 }
 
+// Récupération du formulaire
+const form = document.getElementById("contactForm");
+
+// Gestionnaire d'événement pour le formulaire
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Empêcher l'envoi par défaut du formulaire
+
+  // Validation du formulaire
+  const isFormValid = Validation();
+
+  if (isFormValid) {
+    alert("Votre message a été envoyé avec succès !");
+    console.log("Nom :", form["firstName"].value);
+    console.log("Prénom :", form["last"].value);
+    console.log("E-mail :", form["email"].value);
+    console.log("Message :", form["message"].value);
+
+    // Réinitialiser le formulaire après l'envoi
+    form.reset();
+  } else {
+    alert(
+      "Certaines informations sont incorrectes. Veuillez les corriger et réessayer."
+    );
+  }
+});
+
 // Fonction de validation globale du formulaire
-function validate(form) {
+function Validation() {
   const isFirstNameValid = validateFirst(form);
   const isLastNameValid = validateLast(form);
   const isEmailValid = validateEmail(form);
@@ -111,32 +115,49 @@ function validate(form) {
 
   return isFirstNameValid && isLastNameValid && isEmailValid && isMessageValid;
 }
-function modalContent() {
+
+function Modal() {
   //cree le 2 modal
 
   const modalContent = `
-    <div class="modal-bodyII">
+    <div class="modal_Carre">
     <div class="modal_textII">
       <p>Merci pour votre contact</p>
       <p>Nous reviendrons vers vous dès que possible.</p>
+      </div>
       <input id="closeModal2" type="button" value="Fermer" />
     </div>`;
 
-  const modalBodyII = document.querySelector(".modal-bodyII");
-  modalBodyII.innerHTML = modalContent;
+  const modalCarre = document.querySelector(".modal_Carre");
+  modalCarre.innerHTML = modalContent;
 
-  document
+  /* document
     .getElementById("closeModal2")
-    .addEventListener("click", close_modal2);
+    .addEventListener("click", close_modal2);*/
 }
 // Vérification si le formulaire et les éléments ont les bons identifiants
 document
   .getElementById("btn-submit")
-  .addEventListener("click", function (event) {
+  .addEventListener("submit", function (event) {
     event.preventDefault(); // Empêche la soumission du formulaire par défaut
-    if (validate(form)) {
-      modalContent(); // Afficher la modal si tout est valide
+    if (!Validation()) {
+      modalCarre.style.display = "none";
     } else {
-      alert("Les informations sont incorrectes. Veuillez les corriger.");
+      Modal();
+      modalCarre.style.display = "block";
+      //location.reload(false);
+      alert("Nous avons reçu votre message");
     }
   });
+
+function close_modal(e) {
+  modal_Carre.style.display = "none";
+}
+//close modal 2 avec le rafraîchissement de la page
+function close_modal2() {
+  modal_Carre.style.display = "block";
+  location.reload(false);
+  alert("Nous avons reçu votre message");
+}
+////ecoute  le click de closeModal
+document.getElementById("closeModal").addEventListener("click", close_modal);
